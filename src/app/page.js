bf3d975f0f9b2a7d8542c6bd08ac892adea79c88
nichada <nannichada.j@gmail.com>
2024-02-24 21:44:48 +0700

    add body section

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,23 @@
+import HomeHeader from "@/components/HomeHeader";
+import HomeSearch from "@/components/HomeSearch";
+import Image from 'next/image'
 
 
 export default function Home() {
   return (
-    <h1 class='text-red-500'>
-      Home
-    </h1>
+    <>
+      <HomeHeader />
+      <div className="flex flex-col items-center mt-24">
+        <Image
+          src='https://logolook.net/wp-content/uploads/2021/06/Google-Logo.svg'
+          alt='Google Logo'
+          width={300}
+          height={100}
+          priority
+          style={{ width: 'auto' }}
+        />
+        <HomeSearch />
+      </div>
+    </>
   );
 }
